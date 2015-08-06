7873efaa646310884269d3399438b8917d591e76
MYSun <simonmysun@gmail.com>
2015-08-06 13:09:19 +0800

    Serving for a directory support.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,10 +3,22 @@
         scroller = markmon.scroller,
         contentDisplay = markmon.contentDisplay,
         changeHighlighter = markmon.changeHighlighter;
+    
+    if(!window.location.hash) {
+        window.location.hash = "#/";
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", window.location.hash.slice(1), true);
+        xmlhttp.send();
+    }
 
     var socket = io.connect(location.origin);
     socket.on("content", function(data){
         console.log("got data");
+        if(data.path && window.location.hash.replace("#", "") !== data.path) {
+            console.log('not this page');
+            return;
+        }
         var r = contentDisplay.update(data.html);
         console.log(r);
         if(!r) return;
